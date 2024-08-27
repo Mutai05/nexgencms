162cf5338f82982f319467e203f3e0f346ffbfa8
Mutai05 <kelvok2014@gmail.com>
2024-08-27 15:02:50 +0300

    Upload files media route

--- app/media/routes.py
+++ app/media/routes.py
@@ -10,3 +10,28 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
 
+# Upload files
+
+@media.route('/upload', methods=['GET', 'POST'])
+@login_required
+def upload_file():
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        files = request.files.getlist('files[]')
+
+        if not files:
+            flash('No selected files')
+            return redirect(request.url)
+
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+
+        flash('Files successfully uploaded!', 'success')
+        return redirect(url_for('media.gallery'))
+
+    return render_template('upload.html')
