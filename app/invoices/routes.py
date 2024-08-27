8f37b2c937e4cf27f1c6d2962d5d027f1e2b789d
Mutai05 <kelvok2014@gmail.com>
2024-08-27 15:28:53 +0300

    Invoice route to create a new invoice

--- app/invoices/routes.py
+++ app/invoices/routes.py
@@ -15,3 +15,28 @@ from weasyprint import HTML
 def index():
     invoices_list = Invoice.query.all()
     return render_template('invoice_list.html', invoices=invoices_list)
+
+# Create invoice
+
+@invoices.route('/create', methods=['GET', 'POST'])
+@login_required
+def new():
+    form = InvoiceForm()
+    if form.validate_on_submit():
+        new_invoice = Invoice(
+            invoice_to=form.invoice_to.data,
+            attention=form.attention.data,
+            description=form.description.data,
+            amount=form.amount.data,
+            sub_total=form.sub_total.data,
+            total_amount=form.total_amount.data,
+            due_date=form.due_date.data,
+            is_paid=form.is_paid.data
+        )
+        db.session.add(new_invoice)
+        db.session.commit()
+        
+        flash('Invoice created successfully!', 'success')
+        return redirect(url_for('invoices.index'))
+    
+    return render_template('new_invoice.html', form=form)
