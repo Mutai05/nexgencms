34a91ea054d9f03e917b093752148b51a6851d69
Mutai05 <kelvok2014@gmail.com>
2024-08-27 12:48:40 +0300

    Users route for login

--- app/users/routes.py
+++ app/users/routes.py
@@ -5,11 +5,15 @@ from app import db, bcrypt
 from flask_login import login_user, logout_user, login_required, current_user
 
 
+# List users
+
 @users.route('/')
 @login_required
 def index():
     users = User.query.all()
     return render_template('users/users.html', users=users)
+
+# Register
 
 @users.route('/register', methods=['GET', 'POST'])
 def register():
@@ -40,3 +44,21 @@ def register():
         return redirect(url_for('users.login'))
 
     return render_template('users/register.html')
+
+# Login
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('admin.index'))  # Redirect to the admin index or any other page
+        else:
+            flash('Login failed. Check your email and/or password.', 'danger')
+
+    return render_template('users/login.html')
